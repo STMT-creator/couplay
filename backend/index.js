080e8eb48c32129e8cfe575778640d13b3f7b573
STMT <elahep113@naver.com>
2024-11-22 15:14:26 +0900

    서버 코드 추가 및 변경

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
+import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.sendFile(path.join(__dirname, "notice.html")); // 서버 점검을 알리는 웹문서를 클라이언트에 전송하는 명령
+  // res.send('<h1>Hello World!</h1>')
 })
+
+app.use(express.static('public')) //'public' 자리에 폴더나 파일을 찾는다.
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
